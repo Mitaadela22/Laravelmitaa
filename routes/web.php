aa0d3f5332ecbbdfe37a1f56f7a4760cd60efd06
Mitaadela22 <mitaadela4@gmail.com>
2019-07-09 21:45:36 +0700

    video 28-29

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,8 @@ Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
 			Route::get('/add','UserController@add')->name('admin.user.add')->middleware('akses.admin');
 			Route::post('/add','UserController@save')->middleware('akses.admin');
 
-			Route::get('/add','UserController@add')->name('admin.user.edit')
-					->middleware('akses.admin');
-			Route::post('/add','UserController@update')
-					->middleware('akses.admin');
+			Route::get('/edit/{id}','UserController@edit')->name('admin.user.edit')->middleware('akses.admin');
+			Route::post('/edit/{id}','UserController@update')->middleware('akses.admin');
 			
 			Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
 			Route::post('/setting','UserSettingController@update');
@@ -43,6 +41,7 @@ Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
 
 	Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
 		Route::get('/','KategoriController@daftar')->name('admin.kategori');
+		Route::delete('/','KategoriController@delete');
 		Route::get('/add','KategoriController@add')->name('admin.kategori.add');
 		Route::post('/add','KategoriController@save');
 		Route::get('/edit/{id}','KategoriController@edit')->name('admin.kategori.edit');
